013286f8959c981284e64f81b3a04a2ffcfaebcf
Hyun <hwn123h@naver.com>
2024-02-26 11:37:31 +0900

    단기예보 및 미세먼지

--- src/App.js
+++ src/App.js
@@ -11,24 +11,32 @@ function App() {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [weatherData, setWeatherData] = useState([]);
 
-  const [dustData, setDustData] = useState([]);
+  const [dustData, setDustData] = useState({
+    pm10Value: null,
+    pm25Value: null,
+    dustStatus: null
+  });
 
 
 
   const fetch_WeatherData = async () => {
     let initDate = selectedDate.toISOString().slice(0, 10).replace(/-/g, '');
     const now = new Date();
-    let hour = now.getHours() - 1;
+    let hour = now.getHours();
     hour = String(hour).padStart(2, '0');
 
     const base_time = `${hour}00`;
     console.log(base_time);
 
-    let forecast_url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=W9yTTVsj5jaa3NvKQGwX%2FD4fsm7sdVjTbvLTfj4PaQmK%2BZH5PrEoLtQHTy0vbbRQKhG9go7HNomj%2BpNPslsysw%3D%3D&pageNo=1&numOfRows=50&dataType=json&base_date=${initDate}&base_time=${base_time}&nx=58&ny=127`
+    let forecast_url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=W9yTTVsj5jaa3NvKQGwX%2FD4fsm7sdVjTbvLTfj4PaQmK%2BZH5PrEoLtQHTy0vbbRQKhG9go7HNomj%2BpNPslsysw%3D%3D&pageNo=1&numOfRows=15&dataType=json&base_date=${initDate}&base_time=${base_time}&nx=58&ny=127`
 
     const response = await fetch(forecast_url);
+    if (!response) {
+      console.log("no data now");
+    }
     const data = await response.json();
     const items = data.response.body.items.item;
+
 
     let temp = items.filter(item => item.category === 'TMP');
     let firstTemp = temp.length > 0 ? temp[0] : null;
@@ -47,6 +55,7 @@ function App() {
 
     let wind = items.filter(item => item.category === 'VEC');
     let firstWind = wind.length > 0 ? wind[0] : null;
+    firstWind = firstWind
 
 
     setWeatherData({ firstTemp, firstCloudy, firstHumidity, firstRainy, firstWind });
@@ -56,44 +65,36 @@ function App() {
     fetch_WeatherData();
   }, [selectedDate]);
 
-
-  /*  const fetch_DustData = () => {
-     //시군구별 실시간 평균정보 조회 (경기)
-     let Dustdata_url = 'https://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureSidoLIst?serviceKey=W9yTTVsj5jaa3NvKQGwX%2FD4fsm7sdVjTbvLTfj4PaQmK%2BZH5PrEoLtQHTy0vbbRQKhG9go7HNomj%2BpNPslsysw%3D%3D&returnType=json&numOfRows=100&pageNo=1&sidoName=%EA%B2%BD%EA%B8%B0&searchCondition=DAILY';
- 
-     fetch(Dustdata_url)
-       .then(response => {
-         return response.json();
-       }).then(data => {
-         const items = data.response.body.items;
-         const filteredItems = items.filter(item => item.cityName === '김포시');
-         if (filteredItems.length > 0) {
-           setDustData(filteredItems);
-         } else {
-           console.log("No dust data available");
-         }
-       }).catch(error => {
-         console.error(error);
-       });
-   };
- 
-   useEffect(() => {
-     console.log("Dust data changed, creating table");
-   }, [dustData]); */
-
-  const makeDustTable = () => {
-    const firstData = dustData.length > 0 ? dustData[0] : null;
-    return (
-      <div className='dust-info'>
-        {firstData && (
-          <ul>
-            <li>pm10 μg/m³: {firstData.pm10Value}</li>
-            <li>pm25 μg/m³: {firstData.pm25Value}</li>
-          </ul>
-        )}
-      </div>
-    );
+  function determineDustStatus(pm10Value, pm25Value) {
+    // 미세먼지 지수에 따라 상태를 결정합니다.
+    if (pm10Value <= 30 && pm25Value <= 15) {
+      return '좋음';
+    } else if ((pm10Value <= 80 && pm25Value <= 35) || (pm10Value <= 150 || pm25Value <= 75)) {
+      return '보통';
+    } else if ((pm10Value <= 150 && pm25Value <= 75) || (pm10Value <= 250 && pm25Value <= 150)) {
+      return '나쁨';
+    } else {
+      return '매우 나쁨';
+    }
   }
+
+  const fetch_DustData = async () => {
+    //시군구별 실시간 평균정보 조회 (경기)
+    let Dustdata_url = 'https://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureSidoLIst?serviceKey=W9yTTVsj5jaa3NvKQGwX%2FD4fsm7sdVjTbvLTfj4PaQmK%2BZH5PrEoLtQHTy0vbbRQKhG9go7HNomj%2BpNPslsysw%3D%3D&returnType=json&numOfRows=100&pageNo=1&sidoName=%EA%B2%BD%EA%B8%B0&searchCondition=DAILY';
+    let response = await fetch(Dustdata_url);
+    let data = await response.json();
+    let items = data.response.body.items;
+    const filteredItems = items.filter(item => item.cityName === '김포시');
+    const firstData = filteredItems[0];
+
+    const dustStatus = determineDustStatus(firstData.pm10Value, firstData.pm25Value);
+    setDustData({ ...firstData, dustStatus });
+
+  };
+
+  useEffect(() => {
+    fetch_DustData();
+  }, []);
 
 
   return (
@@ -101,57 +102,15 @@ function App() {
       <div className='card'>
         <div className="weather">
           <h2 className='city'>Weather in 김포</h2>
-          <h1 className="temp">24°C</h1>
+          <h1 className="temp"> {weatherData && weatherData.firstTemp ? weatherData.firstTemp.fcstValue : '불러오는 중...'}°C</h1>
           <img src="" alt="" className='icon' />
           <div className='description'>Cloudy : {weatherData && weatherData.firstCloudy ? weatherData.firstCloudy.fcstValue : '불러오는 중...'} </div>
-          <div className='humidity'>Humidity : {weatherData && weatherData.firstHumidity ? weatherData.firstHumidity.fcstValue : '불러오는 중...'} </div>
-          <div className='rainy'>rainy : {weatherData && weatherData.firstRainy ? weatherData.firstRainy.fcstValue : '불러오는 중...'} </div>
-          <div className="wind">Wind speed : {weatherData && weatherData.firstWind ? weatherData.firstWind.fcstValue : '불러오는 중...'}</div>
+          <div className='humidity'>습도 : {weatherData && weatherData.firstHumidity ? weatherData.firstHumidity.fcstValue : '불러오는 중...'} %</div>
+          <div className='rainy'>강수확률 : {weatherData && weatherData.firstRainy ? weatherData.firstRainy.fcstValue : '불러오는 중...'}  % </div>
+          <div className="wind">풍속 : {weatherData && weatherData.firstWind ? weatherData.firstWind.fcstValue : '불러오는 중...'} m/s</div>
+          <div className="dust">미세먼지: {dustData.dustStatus}</div>
         </div>
       </div>
-      {/* <div className="header">
-        <h1>기상청 날씨 API</h1>
-        <h2>김포시 단기예보 날씨 조회 금일 오전 6시 기준</h2>
-      </div>
-
-      <div className="form-container">
-        <form action="#" onSubmit={(e) => { e.preventDefault(); fetch_WeatherData(); }}>
-          <DatePicker
-            selected={selectedDate}
-            onChange={date => setSelectedDate(date)}
-            dateFormat="yyyyMMdd"
-            minDate={new Date().setDate(new Date().getDate() - 1)}
-            maxDate={new Date()}
-            calendarStartDay={0}
-          />
-          <button type="submit">조회</button>
-        </form>
-      </div>
-
-
-      <div className='container'>
-        <div className='weather-info'>
-          <img src={clear} alt="" />
-          <h1 className='temp'>24°C</h1>
-        </div>
-
-        <div className="temp-list">
-          {weatherData.slice(0, 10).map(item => (
-            <ul key={item.fcstDate + item.fcstTime}>
-              <li>{item.fcstTime}</li>
-              <li>{item.fcstValue}°C</li>
-            </ul>
-          ))}
-        </div>
-
-        <div className='dust-info'>
-          {makeDustTable()}
-        </div>
-
-      </div>
-
-
- */}
 
     </>
   );
